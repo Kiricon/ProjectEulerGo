50b33a9950d70c723c1568dfab99dab3d10098eb
kiricon <kiricon@live.com>
2017-02-11 11:26:41 -0700

    Completed challenge 3

--- 3.go
+++ 3.go
@@ -1,14 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	num := 13195
-
-	for i := num; i > 1; i-- {
+	num := 600851475143
+	upperLimit := int(math.Sqrt(float64(num)))
+	for i := upperLimit; i > 1; i-- {
 		if num%i == 0 {
 			if isPrime(i) {
 				fmt.Println(i)
+				i = 0
 			}
 		}
 	}
@@ -18,9 +22,13 @@ func isPrime(num int) bool {
 
 	if num%2 == 0 {
 		return false
+	} else if num < 3 {
+		return false
 	}
 
-	for i := 3; i < num/2; num += 2 {
+	//upperLimit := int(math.Sqrt(float64(num)))
+
+	for i := 3; i < num/2; i += 2 {
 		if num%i == 0 {
 			return false
 		}
